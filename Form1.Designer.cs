5323138918f994b3e6f34e6d191f9caa1963715f
michaelcent <mikee2285@yahoo.com>
2023-05-24 22:43:20 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿using System;
+
+namespace prayer_timing_app
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+ /*       private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Set the default values for country and city
+            countryCombo.SelectedItem = "Canada";
+            cityCombo.SelectedItem = "Calgary";
+
+            // Perform the initial calculation
+            PerformCalculation();
+        }
+ */
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.countryCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cityCombo = new System.Windows.Forms.ComboBox();
+            this.fajrLabel = new System.Windows.Forms.Label();
+            this.dhuhrLabel = new System.Windows.Forms.Label();
+            this.asrLabel = new System.Windows.Forms.Label();
+            this.maghribLabel = new System.Windows.Forms.Label();
+            this.ishaLabel = new System.Windows.Forms.Label();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mobileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ipadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laptopDesktopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Timer_Tick = new System.Windows.Forms.Timer(this.components);
+            this.muteButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // countryCombo
+            // 
+            this.countryCombo.Font = new System.Drawing.Font("Bookman Old Style", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countryCombo.FormattingEnabled = true;
+            this.countryCombo.Location = new System.Drawing.Point(69, 112);
+            this.countryCombo.Name = "countryCombo";
+            this.countryCombo.Size = new System.Drawing.Size(121, 28);
+            this.countryCombo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "country";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(299, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "city";
+            // 
+            // cityCombo
+            // 
+            this.cityCombo.Font = new System.Drawing.Font("Bookman Old Style", 11F);
+            this.cityCombo.FormattingEnabled = true;
+            this.cityCombo.Location = new System.Drawing.Point(272, 112);
+            this.cityCombo.Name = "cityCombo";
+            this.cityCombo.Size = new System.Drawing.Size(121, 28);
+            this.cityCombo.TabIndex = 3;
+            // 
+            // fajrLabel
+            // 
+            this.fajrLabel.AutoSize = true;
+            this.fajrLabel.Location = new System.Drawing.Point(115, 290);
+            this.fajrLabel.Name = "fajrLabel";
+            this.fajrLabel.Size = new System.Drawing.Size(19, 16);
+            this.fajrLabel.TabIndex = 5;
+            this.fajrLabel.Text = "---";
+            // 
+            // dhuhrLabel
+            // 
+            this.dhuhrLabel.AutoSize = true;
+            this.dhuhrLabel.Location = new System.Drawing.Point(115, 321);
+            this.dhuhrLabel.Name = "dhuhrLabel";
+            this.dhuhrLabel.Size = new System.Drawing.Size(19, 16);
+            this.dhuhrLabel.TabIndex = 6;
+            this.dhuhrLabel.Text = "---";
+            // 
+            // asrLabel
+            // 
+            this.asrLabel.AutoSize = true;
+            this.asrLabel.Location = new System.Drawing.Point(115, 349);
+            this.asrLabel.Name = "asrLabel";
+            this.asrLabel.Size = new System.Drawing.Size(19, 16);
+            this.asrLabel.TabIndex = 7;
+            this.asrLabel.Text = "---";
+            // 
+            // maghribLabel
+            // 
+            this.maghribLabel.AutoSize = true;
+            this.maghribLabel.Location = new System.Drawing.Point(115, 382);
+            this.maghribLabel.Name = "maghribLabel";
+            this.maghribLabel.Size = new System.Drawing.Size(19, 16);
+            this.maghribLabel.TabIndex = 8;
+            this.maghribLabel.Text = "---";
+            // 
+            // ishaLabel
+            // 
+            this.ishaLabel.AutoSize = true;
+            this.ishaLabel.Location = new System.Drawing.Point(115, 407);
+            this.ishaLabel.Name = "ishaLabel";
+            this.ishaLabel.Size = new System.Drawing.Size(19, 16);
+            this.ishaLabel.TabIndex = 9;
+            this.ishaLabel.Text = "---";
+            // 
+            // datePicker
+            // 
+            this.datePicker.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datePicker.Location = new System.Drawing.Point(44, 40);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(386, 39);
+            this.datePicker.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.optionToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(468, 28);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // optionToolStripMenuItem
+            // 
+            this.optionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mobileToolStripMenuItem,
+            this.ipadToolStripMenuItem,
+            this.laptopDesktopToolStripMenuItem});
+            this.optionToolStripMenuItem.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.optionToolStripMenuItem.Text = "Option";
+            // 
+            // mobileToolStripMenuItem
+            // 
+            this.mobileToolStripMenuItem.Name = "mobileToolStripMenuItem";
+            this.mobileToolStripMenuItem.Size = new System.Drawing.Size(245, 26);
+            this.mobileToolStripMenuItem.Text = "Mobile";
+            // 
+            // ipadToolStripMenuItem
+            // 
+            this.ipadToolStripMenuItem.Name = "ipadToolStripMenuItem";
+            this.ipadToolStripMenuItem.Size = new System.Drawing.Size(245, 26);
+            this.ipadToolStripMenuItem.Text = "tablet";
+            // 
+            // laptopDesktopToolStripMenuItem
+            // 
+            this.laptopDesktopToolStripMenuItem.Name = "laptopDesktopToolStripMenuItem";
+            this.laptopDesktopToolStripMenuItem.Size = new System.Drawing.Size(245, 26);
+            this.laptopDesktopToolStripMenuItem.Text = "Laptop / desktop";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // muteButton
+            // 
+            this.muteButton.Location = new System.Drawing.Point(340, 407);
+            this.muteButton.Name = "muteButton";
+            this.muteButton.Size = new System.Drawing.Size(75, 23);
+            this.muteButton.TabIndex = 12;
+            this.muteButton.Text = "button1";
+            this.muteButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Fajr";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Dhuhr";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 349);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Asr";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 382);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Maghrib";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(51, 407);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 16);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Isha";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.muteButton);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.ishaLabel);
+            this.Controls.Add(this.maghribLabel);
+            this.Controls.Add(this.asrLabel);
+            this.Controls.Add(this.dhuhrLabel);
+            this.Controls.Add(this.fajrLabel);
+            this.Controls.Add(this.cityCombo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.countryCombo);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox countryCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cityCombo;
+        private System.Windows.Forms.Label fajrLabel;
+        private System.Windows.Forms.Label dhuhrLabel;
+        private System.Windows.Forms.Label asrLabel;
+        private System.Windows.Forms.Label maghribLabel;
+        private System.Windows.Forms.Label ishaLabel;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mobileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ipadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laptopDesktopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Timer Timer_Tick;
+        private System.Windows.Forms.Button muteButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using OfficeOpenXml;
+using System.Linq;
+using System.Media;
+
+namespace prayer_timing_app
+{
+    public partial class MainForm : Form
+    {
+        private string excelFilePath = "C:\\Users\\M.Centeno\\Desktop\\prayer_timing_app\\worldcities.xlsx";
+        private string audioFilePath = "C:\\Users\\M.Centeno\\Desktop\\prayer_timing_app\\PrayerAlarm.wma";
+
+        private ExcelPackage excelPackage;
+        private ExcelWorksheet worksheet;
+        private bool isMuted = false;
+        private SoundPlayer alarmSoundPlayer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            excelPackage = new ExcelPackage(new FileInfo(excelFilePath));
+            worksheet = excelPackage.Workbook.Worksheets[0];
+            PopulateCountryComboBox();
+            datePicker.ValueChanged += DateTimePicker_ValueChanged;
+            countryCombo.SelectedIndexChanged += CountryCombo_SelectedIndexChanged;
+            cityCombo.SelectedIndexChanged += CityCombo_SelectedIndexChanged;
+            muteButton.Click += MuteButton_Click;
+            this.FormClosing += windowLabel_FormClosing;
+
+            //load alarm
+            alarmSoundPlayer = new SoundPlayer(audioFilePath);
+
+            // Set the default values for country and city
+            countryCombo.SelectedItem = "Canada";
+            cityCombo.SelectedItem = "Calgary";
+
+            
+            PerformCalculation();
+        }
+
+        private void PopulateCountryComboBox()
+        {
+            var countries = worksheet.Cells[2, 5, worksheet.Dimension.Rows, 5]
+                .Select(cell => cell.Value.ToString())
+                .Distinct()
+                .OrderBy(country => country)
+                .ToList();
+            countryCombo.Items.AddRange(countries.ToArray());
+        }
+
+        private void CountryCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedCountry = countryCombo.SelectedItem.ToString();
+            var cities = worksheet.Cells[2, 1, worksheet.Dimension.Rows, 1]
+                .Where(cell => cell.Offset(0, 4).Value.ToString() == selectedCountry)
+                .Select(cell => cell.Value.ToString())
+                .Distinct()
+                .OrderBy(city => city)
+                .ToList();
+            cityCombo.Items.Clear();
+            cityCombo.Items.AddRange(cities.ToArray());
+
+            // Updating city ny user
+            if (cities.Contains(cityCombo.SelectedItem))
+            {
+                // deafult city whenever program is restarted
+                cityCombo.SelectedItem = cityCombo.SelectedItem;
+            }
+            else
+            {
+                // default the city for calgary to be called
+                cityCombo.SelectedItem = cities.FirstOrDefault();
+            }
+
+            //restart
+            PerformCalculation();
+        }
+
+        private void CityCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           //restart 
+            PerformCalculation();
+        }
+
+        private void DateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            //restart
+            PerformCalculation();
+        }
+
+        private void PerformCalculation()
+        {
+            DateTime selectedDate = datePicker.Value;
+            string selectedCountry = countryCombo.SelectedItem.ToString();
+            string selectedCity = cityCombo.SelectedItem.ToString();
+            double longitude = GetLongitude(selectedCountry, selectedCity);
+            double latitude = GetLatitude(selectedCountry, selectedCity);
+
+            string apiUrl = $"https://api.aladhan.com/v1/timings/{selectedDate.Date:yyyy-MM-dd}?latitude={latitude}&longitude={longitude}&method=12";
+
+            string apiResponse;
+            using (WebClient client = new WebClient())
+            {
+                apiResponse = client.DownloadString(apiUrl);
+            }
+
+            JObject json = JObject.Parse(apiResponse);
+            JToken timings = json["data"]["timings"];
+            string fajr = timings["Fajr"].ToString();
+            string sunrise = timings["Sunrise"].ToString();
+            string dhuhr = timings["Dhuhr"].ToString();
+            string asr = timings["Asr"].ToString();
+            string maghrib = timings["Maghrib"].ToString();
+            string isha = timings["Isha"].ToString();
+
+            fajrLabel.Text = fajr;
+            dhuhrLabel.Text = dhuhr;
+            asrLabel.Text = asr;
+            maghribLabel.Text = maghrib;
+            ishaLabel.Text = isha;
+
+            // Play the alarm 
+            string currentTime = DateTime.Now.ToString("HH:mm");
+            if (!isMuted && (currentTime == fajr || currentTime == dhuhr || currentTime == asr || currentTime == maghrib || currentTime == isha))
+            {
+                alarmSoundPlayer.Play();
+            }
+        }
+
+        private double GetLongitude(string country, string city)
+        {
+            return GetCoordinateValue(country, city, 4);
+        }
+
+        private double GetLatitude(string country, string city)
+        {
+            return GetCoordinateValue(country, city, 3);
+        }
+
+        private double GetCoordinateValue(string country, string city, int column)
+        {
+            int rowIndex = 2;
+
+            while (worksheet.Cells[rowIndex, 5].Value != null)
+            {
+                string cellCountry = worksheet.Cells[rowIndex, 5].Value.ToString();
+                string cellCity = worksheet.Cells[rowIndex, 1].Value.ToString();
+
+                if (cellCountry == country && cellCity == city)
+                {
+                    double coordinate;
+                    if (double.TryParse(worksheet.Cells[rowIndex, column].Value.ToString(), out coordinate))
+                    {
+                        return coordinate;
+                    }
+                }
+                rowIndex++;
+            }
+            throw new Exception("Coordinate not found.");
+        }
+
+        private void MuteButton_Click(object sender, EventArgs e)
+        {
+            isMuted = !isMuted;
+            muteButton.Text = isMuted ? "Unmute" : "Mute";
+        }
+
+        private void windowLabel_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            excelPackage.Dispose();
+            alarmSoundPlayer.Dispose();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // setting up the default city and country 
+            countryCombo.SelectedItem = "Canada";
+            cityCombo.SelectedItem = "Calgary";
+
+            // restet
+            PerformCalculation();
+        }
+    }
+}
